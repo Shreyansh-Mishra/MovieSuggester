d27517806d59d6ba842832c14947017f40cb8ec5
Shreyansh-Mishra <201951146@iiitvadodara.ac.in>
2021-06-24 01:28:28 +0530

    fix issue in the navbar search

--- src/Movies.js
+++ src/Movies.js
@@ -3,67 +3,13 @@ import { useParams } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import { useHistory } from 'react-router'
 export default function Movies(props) {
-    let history = useHistory()
-    let name = useParams()
-    let [movieData,setMovieData]=useState(null)
-    let [isPending,setIsPending]=useState(true)
-    let [isError, setIsError] = useState(false)
-    const mystyle={
-        "margin-left":"50px"
-    }
-    let [mname,setmname] = useState((window.location.href).split('/')[4])
-    useEffect(()=>{ 
-        setmname((window.location.href).split('/')[4])    
-        fetch(`http://www.omdbapi.com/?t=${mname}&apikey=e420a177`).then((res)=>{
-        res.json().then((p)=>{
-          console.log("p: "+p.Response)
-          if(p.Response==='False'){
-            setIsError(true)
-          }
-            setMovieData(p)
-            setIsPending(false)
-        })
+  let history = useHistory();
+  let {name} = useParams();
+    useEffect(()=>{
+      history.push(`/movie/${name}`)
     })
-    },[])
-
-    let handleDownload = () =>{
-      history.push(`/download/${movieData.Title}`)
-    }
-
-    console.log(movieData)
     return (
-        <div>
-          {console.log("error ye rha:"+isError)}
-            {isPending&&<div>Loading...</div>}
-            {!isError&&movieData&&<div class="card" style={{"background-color":"#D2C3C3"}}>
-  <div class="card__inner" >
-    
-    <main class="card__body">
-    <img src={`${movieData.Poster}`} height="25%" width="25%" style={mystyle}></img>
-      <div class="card__info">
-        <h1 class="card__title">{movieData.Title}</h1>
-    
-        <p class="card__slug">{movieData.Plot}</p>
-      
-        <button class="card__btn" value="Download" onClick={handleDownload}>Download</button>
-
-        <div class="card__rating">
-          {movieData.imdbRating}
-        </div>
-      </div>
-    </main>
-    
-    <footer class="card__footer">
-      <ul class="list list--info" style={{"margin-right":"30px"}}>
-        <li>{movieData.Released}</li>
-        <li>{movieData.Runtime}</li>
-        <li>{movieData.Genre}</li>
-      </ul>
-    </footer>
-  </div>
-</div>}
-{isError&&<center><div>Movie Not Found</div></center>}
-        </div>
+        <div></div>
     )
     
     
